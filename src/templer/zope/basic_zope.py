2eb33500a3608693a1e190c3a1e9df4923de276c
cewing <cewing@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2011-05-18 15:54:35 +0000

    port abstract and basic zope from zopeskel, updating imports on the way
    git-svn-id: https://svn.plone.org/svn/collective/templer.zope/trunk@239503 db7f04ef-aaf3-0310-a811-c281ed44c4ad

---
+++ src/templer/zope/basic_zope.py
@@ -0,0 +1,19 @@
+import copy 
+from templer.core.base import get_var
+from templer.zope import abstract_zope
+
+
+class BasicZope(abstract_zope.AbstractZope):
+    _template_dir = 'templates/basic_zope'
+    summary = "A Zope project"
+    help = """
+This creates a basic Zope project.
+"""
+    required_templates = ['basic_namespace']
+    use_cheetah = True
+
+    vars = copy.deepcopy(abstract_zope.AbstractZope.vars)
+    get_var(vars, 'namespace_package').default = 'myzopelib'
+    get_var(vars, 'package').default = 'example'
+
+
